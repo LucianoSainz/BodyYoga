bcd31e1112210fe15f6bbd8eb7ec39bd13fcdd4d
danielsainz <danielsainz28@gmail.com>
2020-08-03 17:49:56 +0200

    added menu with javascript

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let btnMenu = document.querySelector('.btn-menu');
+let menu = document.querySelector('.list-container');
+let on_off = true;
+
+btnMenu.addEventListener('click', () => {
+    if(on_off) {
+       menu.style.left = 0%;
+       menu.style.tramsition = "0.5s";
+
+       on_off = false;
+    }
+});
