8ad7f54ca7dd9483b8ee88f42341dd77534e8b1c
danielsainz <danielsainz28@gmail.com>
2020-08-03 18:06:20 +0200

    finish the button that displays the menu in js

--- main.js
+++ main.js
@@ -5,9 +5,16 @@ let on_off = true;
 
 btnMenu.addEventListener('click', () => {
     if(on_off) {
-       menu.style.left = 0%;
+       menu.style.left = "0%";
        menu.style.tramsition = "0.5s";
 
        on_off = false;
+
+    }else{
+        on_off = false;
+       menu.style.left = "100%";
+       menu.style.tramsition = "0.5s";
+
+       on_off = true;
     }
 });
