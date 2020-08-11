79da7f37f57e141448c249c75976a963b185ee81
danielsainz <danielsainz28@gmail.com>
2020-08-11 19:35:14 +0200

    added social footer

--- js/main.js
+++ js/main.js
@@ -21,3 +21,18 @@ btnMenu.addEventListener('click', () => {
        on_off = true;
     }
 });
+
+//scroll
+
+$(document).ready(function(){
+	var scrollLink = $('.scroll');
+
+	//Smooth scrolling
+
+	scrollLink.click(function(e){
+		e.preventDefault();
+		$('body, html').animate({
+		scrollTop:	$(this.hash).offset().top
+		}, 2000)
+	});
+})
